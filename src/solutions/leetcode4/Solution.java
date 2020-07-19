74d1401fa965c1544f59715cb9fe7adf540cd651
puneethcshetty <60575788+puneethcshetty@users.noreply.github.com>
2020-07-19 12:50:35 -0400

    Update

--- src/solutions/leetcode4/Solution.java
+++ src/solutions/leetcode4/Solution.java
@@ -1,42 +1,43 @@
 package solutions.leetcode4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int mid = (nums1.length + nums2.length)/2;
-        int[] num3 = new int[mid+1];
+        List<Integer> list = new ArrayList<>();
         int count = 0,i = 0, j = 0;
         double result;
         while(nums1.length != i && nums2.length != j){
             if(count == (mid+1))
                 break;
             if(nums1[i] < nums2[j]){
-                num3[count] = nums1[i];
+                list.add(nums1[i]);
                 i++;
             }
             else{
-                num3[count] = nums2[j];
+                list.add(nums2[j]);
                 j++;
             }
             count++;
         }
-        if(count != (mid+1)){
-            while(nums1.length != i){
-                if(count == (mid+1))
-                    break;
-                num3[count] = nums1[i];
-                i++;count++;
-            }
-            while(nums2.length != j){
-                if(count == (mid+1))
-                    break;
-                num3[count] = nums2[j];
-                j++;count++;
-            }
+        while(nums1.length != i){
+            if(count == (mid+1))
+                break;
+            list.add(nums1[i]);
+            i++;count++;
+        }
+        while(nums2.length != j){
+            if(count == (mid+1))
+                break;
+            list.add(nums2[j]);
+            j++;count++;
         }
         if((nums1.length + nums2.length)%2 == 0)
-            result = (num3[mid]+num3[mid-1])/2.0;
+            result = (list.get(mid)+list.get(mid-1))/2.0;
         else
-            result = num3[mid];
+            result = list.get(mid);
         return result;
     }
 
